7b438f2a8cda0cfe7af736ff3d93334d970e0e14
giuliogallo94 <giuliogallo1994@gmail.com>
2023-11-23 13:58:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+  <h1>Hello</h1>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+</style>
